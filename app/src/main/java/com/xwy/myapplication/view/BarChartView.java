9d0956eb4a6b0db9f01c537ef984202a0e3d049f
Xuweiyu <1847449751@qq.com>
2019-02-16 00:02:49 +0800

    第一次提交

---
+++ app/src/main/java/com/xwy/myapplication/view/BarChartView.java
@@ -0,0 +1,24 @@
+package com.xwy.myapplication.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2019/2/12.
+ */
+
+public class BarChartView extends View {
+    public BarChartView(Context context) {
+        super(context);
+    }
+
+    public BarChartView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BarChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
